5217b8e9d5fcca5155d4c4107327606372b3ec97
Drinktea215 <kolomvbok215@gmail.com>
2024-09-15 12:32:52 +0600

    Fixed task 1

--- task 1.py
+++ task 1.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
+from sqlalchemy.dialects.sqlite import JSON
 from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
-from sqlalchemy import Integer, DateTime, String, func, ForeignKey, Table, Column
+from sqlalchemy import Integer, DateTime, String, func, ForeignKey
 from datetime import datetime
-from random import choice
+from typing import Optional
 from enum import Enum
 from sqlalchemy import Enum as SAEnum
 
@@ -11,52 +12,41 @@ class Base(DeclarativeBase):
     pass
 
 
-assoc_table_for_boosts = Table(
-    "assoc_table_for_boosts", Base.metadata,
-    Column("player_id", ForeignKey("players.id", ondelete="CASCADE"), primary_key=True),
-    Column("boost_id", ForeignKey("boosts.id", ondelete="CASCADE"), primary_key=True)
-)
-
-
 class Player(Base):
     __tablename__ = "players"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String, unique=True)
     start_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    level: Mapped[int] = mapped_column(Integer, default=1)
-    experience: Mapped[int] = mapped_column(Integer, default=0)
-    experience_for_level_up: Mapped[int] = mapped_column(Integer, default=1000)
-    count_reward_for_level_up: Mapped[int] = mapped_column(Integer, default=1)
+    level_completed: Mapped[list[str]] = mapped_column(JSON)
 
-    boosts: Mapped[list[Boost]] = relationship(secondary=assoc_table_for_boosts, back_populates="players")
+    boosts: Mapped[Optional[list[PlayerBoost]]] = relationship('PlayerBoost', back_populates='player')
 
-    def _add_boost(self, type_boost: str, count=None):
-        count = self.count_reward_for_level_up if count is None else count
-        for x in range(count):
+    def __init__(self, username: str):
+        self.username = username
+        self.level_completed = []
+
+    def _add_boost(self, session: Session, type_boost: str, count: int = 1):
+        boost = session.query(Boost).filter_by(type=type_boost).first()
+        if not boost:
             boost = Boost(type=type_boost)
-            self.boosts.append(boost)
+            session.add(boost)
+            session.commit()
 
-    def level_up(self):
-        self.level += 1
-        self.experience = 0
-        self.experience_for_level_up *= 2
-        self._add_boost_for_level_up()
-        self.count_reward_for_level_up += 1
+        existing_player_boost = next((b for b in self.boosts if b.boost == boost), None)
+        if existing_player_boost:
+            existing_player_boost.count += count
+        else:
+            player_boost = PlayerBoost(player=self, boost=boost, count=count)
+            self.boosts.append(player_boost)
 
-    def _add_boost_for_level_up(self):
-        type_boost = choice(list(BoostType))
-        self._add_boost(type_boost.value)
+    def add_boost_for_level(self, session: Session, level: str, type_boost: str, count: int):
+        if level not in self.level_completed:
+            self.level_completed.append(level)
+        self._add_boost(session, type_boost, count)
 
-    def add_boost_not_level_up(self, type_boost, count):
-        self._add_boost(type_boost, count)
-
-    def increase_experience(self, amount: int):
-        self.experience += amount
-        while self.experience >= self.experience_for_level_up:
-            amount = self.experience - self.experience_for_level_up
-            self.level_up()
-            self.experience += amount
+    def add_boost_not_level(self, session: Session, type_boost, count):
+        self._add_boost(session, type_boost, count)
 
 
 class BoostType(Enum):
@@ -70,6 +60,17 @@ class Boost(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     type: Mapped["BoostType"] = mapped_column(SAEnum(BoostType))
-    players: Mapped[list[Player]] = relationship(secondary=assoc_table_for_boosts,
-                                                 back_populates="boosts",
-                                                 lazy="selectin")
+    players: Mapped[Optional[list[PlayerBoost]]] = relationship('PlayerBoost', back_populates='boost')
+
+
+class PlayerBoost(Base):
+    __tablename__ = 'player_boosts'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    player_id: Mapped[str] = mapped_column(Integer, ForeignKey('players.id'))
+    player: Mapped[Optional[list[Player]]] = relationship('Player', back_populates='boosts', cascade="all")
+    boost_id: Mapped[int] = mapped_column(Integer, ForeignKey('boosts.id'))
+    boost: Mapped[Optional[list[Boost]]] = relationship('Boost', back_populates='players', cascade="all")
+
+    count: Mapped[int] = mapped_column(Integer, default=0)
